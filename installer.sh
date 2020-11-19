71cc1e60fa084aa842339ffa5c7ffeb1fb21d7e6
amit.kremer <amit.kremer@appsflyer.com>
2020-11-19 09:22:30 +0200

    add icons

--- installer.sh
+++ installer.sh
@@ -1,9 +1,8 @@
-echo "Do you want the Strict version of AppsFlyer react-native SDK? (no IDFA collection)[y/n]"
+echo "📦 Do you want the Strict version of AppsFlyer react-native SDK? (no IDFA collection)[y/n]"
 read ANSWER
 if [[ \"$ANSWER\" = \"y\" ||  \"$ANSWER\" = \"Y\" ]]; then
-    echo "You choose Strict mode"
-    cat podspec-strict-template.txt > react-native-appsflyer.podspec
+    echo "You choose Strict mode ✅"
+    cat podspec-strict-template.txt > npmtestforbm.podspec
 else
-  echo "You choose Main mode"
+  echo "You choose Regular mode ✅"
 fi
-
