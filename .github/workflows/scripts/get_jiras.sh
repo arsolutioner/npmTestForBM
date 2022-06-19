8f33ddd1eb8129e77f8863066baa2a0ea53300d3
Amit <Amit.kremer@appsflyer.com>
2022-06-19 11:38:25 +0300

    dddsd

---
+++ .github/workflows/scripts/get_jiras.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ # jira_token="bWF4aW1AYXBwc2ZseWVyLmNvbTp3Nk1OaGJSYUZsZnNhMXBiOEZkdzU3MDU"
+fixed_version_found=false
+fixed_version=$(echo "${{github.ref_name}}" | grep -Eo '[0-9].[0-9].[0-9]+')
+jira_fixed_version="React-Native SDK v$fixed_version"
+curl -X GET -H "Authorization: Basic $JIRA_TOKEN=" https://appsflyer.atlassian.net/rest/api/3/project/11400/versions | jq -r '.[] | .name+""+.id' | while read version ; do
+    if [[ "$version" == *"$jira_fixed_version"* ]];then
+        echo "${jira_fixed_version} Found!"
+        fixed_version_found=true
+        version_id=${version#"$jira_fixed_version"}
+        echo -e $(curl -X GET -H "Authorization: Basic $JIRA_TOKEN=" https://appsflyer.atlassian.net/rest/api/3/search?jql=fixVersion=$version_id | jq -r '.issues[] | "- " + .fields["summary"]+"@"') > "$jira_fixed_version-releasenotes".txt
+        sed -i -r -e "s/@/\n/gi" "$jira_fixed_version-releasenotes".txt
+    fi
+    done
+if [ fixed_version_found == false ];then
+    echo "${jira_fixed_version} is not found!"
+    exit 1
+fi
+cat "$jira_fixed_version-releasenotes".txt
