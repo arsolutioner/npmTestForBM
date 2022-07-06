577cb4983b368048c5d3e7166bdd05ff21491247
Amit <Amit.kremer@appsflyer.com>
2022-07-06 16:52:17 +0300

    11

--- .github/workflows/scripts/decryptSecrets.sh
+++ .github/workflows/scripts/decryptSecrets.sh
@@ -9,11 +9,12 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/GithubCIApp.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/GithubCIApp.mobileprovision.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-# security import ./.github/secrets/GithubCICer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "$IOS_KEYS" build.keychain
+echo "here we are fine..."
+security import ./.github/secrets/GithubCICer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
-# security list-keychains -s ~/Library/Keychains/build.keychain
-# security default-keychain -s ~/Library/Keychains/build.keychain
-# security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "$IOS_KEYS" ~/Library/Keychains/build.keychain
 
-# security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "$IOS_KEYS" ~/Library/Keychains/build.keychain
