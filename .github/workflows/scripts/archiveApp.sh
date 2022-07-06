8ca340cfb0624a81db6ac811434614410a310cf6
Amit <Amit.kremer@appsflyer.com>
2022-07-06 17:21:22 +0300

    13

--- .github/workflows/scripts/archiveApp.sh
+++ .github/workflows/scripts/archiveApp.sh
@@ -5,6 +5,5 @@ set -eo pipefail
 xcodebuild -workspace AppsFlyerExample.xcworkspace \
             -scheme AppsFlyerExample \
             -sdk iphoneos \
-            -configuration AppStoreDistribution \
             -archivePath $PWD/build/AppsFlyerExample.xcarchive \
             clean archive | xcpretty
