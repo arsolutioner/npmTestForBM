39c600c368d7838c2fb5db9c1c91c7bfbb657279
Amit <Amit.kremer@appsflyer.com>
2022-07-06 16:47:00 +0300

    9

--- .github/workflows/scripts/decryptSecrets.sh
+++ .github/workflows/scripts/decryptSecrets.sh
@@ -9,8 +9,8 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/GithubCIApp.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/GithubCIApp.mobileprovision.mobileprovision
 
 
-# security create-keychain -p "" build.keychain
-# security import ./.github/secrets/GithubCICer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/GithubCICer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 # security list-keychains -s ~/Library/Keychains/build.keychain
 # security default-keychain -s ~/Library/Keychains/build.keychain
