b3686266a16e76d6202f65acbdd622291336bd99
Amit <Amit.kremer@appsflyer.com>
2022-07-06 16:45:23 +0300

    8

--- .github/workflows/scripts/decryptSecrets.sh
+++ .github/workflows/scripts/decryptSecrets.sh
@@ -9,11 +9,11 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/GithubCIApp.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/GithubCIApp.mobileprovision.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/GithubCICer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+# security create-keychain -p "" build.keychain
+# security import ./.github/secrets/GithubCICer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+# security list-keychains -s ~/Library/Keychains/build.keychain
+# security default-keychain -s ~/Library/Keychains/build.keychain
+# security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+# security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
